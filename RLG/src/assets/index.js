cc0bb6bb56f38d3e77f28cc58d4faaec1929ce95
Kevin-The-Cyber-Coder <kevineniyomurinzi@gmail.com>
2026-05-25 13:23:07 +0200

    upgrdes

---
+++ RLG/src/assets/index.js
@@ -0,0 +1,61 @@
+// Logo and Brand Images
+import logo from './logo.png';
+import footerLogo from './footer-logo.png';
+
+// Hero Images
+import heroBg from './hero-bg.jpg';
+import heroImage from './hero-image.png';
+
+// Background Patterns
+import patternBg from './pattern-bg.svg';
+import dotsPattern from './dots-pattern.png';
+import waveBg from './wave-bg.svg';
+
+// About Section Images
+import aboutImage from './about-image.jpg';
+
+// Program Icons
+import programIcon1 from './program-icon-1.png';
+import programIcon2 from './program-icon-2.png';
+import programIcon3 from './program-icon-3.png';
+
+// Testimonial Images
+import testimonial1 from './testimonial-1.jpg';
+import testimonial2 from './testimonial-2.jpg';
+import testimonial3 from './testimonial-3.jpg';
+
+// Team Images
+import team1 from './team-1.jpg';
+import team2 from './team-2.jpg';
+import team3 from './team-3.jpg';
+import team4 from './team-4.jpg';
+
+// Event Images
+import event1 from './event-1.jpg';
+import event2 from './event-2.jpg';
+import event3 from './event-3.jpg';
+
+// Export all images
+export {
+  logo,
+  footerLogo,
+  heroBg,
+  heroImage,
+  patternBg,
+  dotsPattern,
+  waveBg,
+  aboutImage,
+  programIcon1,
+  programIcon2,
+  programIcon3,
+  testimonial1,
+  testimonial2,
+  testimonial3,
+  team1,
+  team2,
+  team3,
+  team4,
+  event1,
+  event2,
+  event3,
+};
